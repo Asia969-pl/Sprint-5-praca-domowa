501b20049ccf4605f2d99d1bea00bbb0b287e2b1
Asia969-pl <joanna_pagowska@o2.pl>
2025-03-30 16:15:44 +0200

    Update zadanie.js

--- zadanie.js
+++ zadanie.js
@@ -130,29 +130,6 @@ const createMostCommonLetter = createPersonAge.map((person) => {
 
 console.log(createMostCommonLetter);
 
-// Stwórz funkcję,
-// która analizuje tablicę osób z drugiego zadania i znajduje najczęściej występującą literę w polach
-// firstName, lastName oraz nickname dla każdej osoby. Wynik powinien zawierać literę i jej liczbę wystąpień,
-// a także określić, co zrobić w przypadku, gdy dwie litery mają tę samą liczbę wystąpień.
 
-// Wytyczne:
 
-// Przetwarzaj każdy obiekt osoby, analizując pola firstName, lastName, i nickname.
 
-// Zliczaj wystąpienia każdej litery w tych polach i znajdź najczęściej występującą literę.
-
-// Jeżeli dwie lub więcej liter mają tę samą liczbę wystąpień i jest to najwyższa wartość, wybierz literę,
-// która występuje pierwsza w alfabecie.
-
-// Zwróć nową tablicę z obiektami, które zawierają imię, nazwisko, pseudonim oraz dodaj nową właściwość
-// do każdej osoby o nazwie mostCommonLetter, której wartością będzie obiekt z kluczem litery oraz ilością jej wystąpień.
-// function countValue(obj) {
-//   const result ={};
-//   const values = Object.values(obj);
-//   for (let i = 0; i < values.length; i++) {
-//       const value = values[i];
-//       result[value] = result[value]? result[value] +1 : 1
-//   }
-//   return result;
-// }
-// console.log(countValue(test));
